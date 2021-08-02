5581549483fcf131dd6b733f9517df2830d8b184
Christian Zagorski <christianz@hey.com>
2021-08-02 11:43:25 -0700

    Initial Commit

---
+++ Shared/Class_property_sharingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Class_property_sharingApp.swift
+//  Shared
+//
+//  Created by Christian Zagorski on 8/2/21.
+//
+
+import SwiftUI
+
+@main
+struct Class_property_sharingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
